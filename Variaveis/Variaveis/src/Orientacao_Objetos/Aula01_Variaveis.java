be021425050a4c77e23d66dafc803903e1ed7420
Shaaman331 <tarcisio2010souza@gmail.com>
2023-10-27 12:32:45 -0300

    Terceiro Commit

---
+++ Variaveis/Variaveis/src/Orientacao_Objetos/Aula01_Variaveis.java
@@ -0,0 +1,16 @@
+package Orientacao_Objetos;
+
+public class Aula01_Variaveis {
+    public static void main(String[] args) {
+        int idade = 30; 
+        String nome = "Tarcisio";
+        String nomeDoMeuCachorro = "Loki";
+
+    System.out.println(idade);
+    System.out.println(nome);
+    System.out.println(nomeDoMeuCachorro);
+    
+
+    }
+    
+}
