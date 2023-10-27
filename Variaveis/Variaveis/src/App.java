180628e0f86846359f1414e8c63396420b751fbf
Shaaman331 <tarcisio2010souza@gmail.com>
2023-10-27 11:01:23 -0300

    Primeiro Commit

---
+++ Variaveis/Variaveis/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //tipo nome
+        //Variaveis do tipo inteiro
+
+        byte numeroPequeno = 125;
+        short numeroShort = 32767;
+        int numeroMaior = 2147483647;
+        long numeroEnorme = 9223372036685477000l;
+        
+        System.out.println(numeroPequeno);
+        System.out.println(numeroShort);
+        System.out.println(numeroMaior);
+        System.out.println(numeroEnorme);
+
+        //Variaveis do tipo Flutuante
+        float peso = 78.2f; 
+        double pi = 3.14159265358979323846;
+
+        System.out.println(peso);
+        System.out.println(pi);
+
+        //Variaveis do tipo caracter
+        //variaiveis do tipo char guardam um único valor ]
+        //boolean estouComFome = false;    
+      
+        //Variaveis do tipo string
+        String nome = "Tarcisio";
+        System.out.println(nome);
+
+    }
+}
---
+++ Variaveis/Variaveis/src/Aula05.java
@@ -0,0 +1,44 @@
+public class Aula05 {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto; //Declarando a variável
+        nomeCompleto = "Uzumaki Naruto"; //Inicializando a variável
+        System.out.println(nomeCompleto); //Usando a variável
+
+        String filme = "Vingadores";
+        int anoLancamento = 2015;
+        int duracao = 120;
+        float notaCritica = 8.7f;
+        char letraInicial = 'V';
+        boolean foiSucesso= true;
+
+        System.out.println(filme);
+        System.out.println(anoLancamento);
+        System.out.println(duracao);
+        System.out.println(notaCritica);
+        System.out.println(letraInicial);
+        System.out.println(foiSucesso);
+
+       //O filme <filme> lançado em <anos> tem uma duração de <duração> min.
+        System.out.println("O filme" + filme + "lançado em " + anoLancamento + "tem uma duração de" + duracao + "minutos.");
+        System.out.format("O filme %s lançado em %d tem uma duração de %d minutos \n", filme, anoLancamento, duracao);
+
+        String texto = "O filme" + filme + "lançado em" + anoLancamento + "tem uma duração de" + duracao + "minutos.";
+        String texto2 = String.format("O filme %s lançado em %d tem uma duração de %d minutos \n", filme, anoLancamento, duracao);
+
+        System.out.println(texto);
+        System.out.println(texto2);
+
+        System.out.println("\n");
+
+        System.out.println("Programa 02");
+        String mensagem01 = "Olá, como está ?";
+        String mensagem02 = "Espero que esteja bem";
+        String mensagem03 = "Beijos até mais :D";
+
+        String mensagemFinal =  String.format("%s %s. %s", mensagem01, mensagem02, mensagem03);
+        System.out.println(mensagemFinal);
+
+
+    }
+
+}
